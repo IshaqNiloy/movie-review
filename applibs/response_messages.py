8b36985b4d5161626f47455b53dc251a43365bb3
IshaqNiloy <ishaqniloy1996@gmail.com>
2023-05-17 17:08:14 +0600

    Implement api for movie delete

---
+++ applibs/response_messages.py
@@ -0,0 +1,8 @@
+# general
+INVALID_REQUEST_DATA = 'Invalid request data'
+
+# db
+MOVIE_SAVE_SUCCESS = 'Movie saved successfully'
+MOVIE_SAVE_FAILED = 'Movie save failed'
+MOVIE_DELETE_SUCCESS = 'Movie deleted successfully'
+MOVIE_DELETE_FAILED = 'Movie delete failed'
