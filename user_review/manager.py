34a91936cbc4814c3557791b257b2da804573b57
IshaqNiloy <ishaqniloy1996@gmail.com>
2023-10-11 00:23:02 +0600

    code refactor

--- user_review/manager.py
+++ user_review/manager.py
@@ -10,57 +10,18 @@ logger = logging.getLogger(__name__)
 
 class UserReviewManager(models.Manager):
     def add_review(self, movie_obj, user_obj, review: str):
-        try:
-            data = self.create(movie=movie_obj, user=user_obj, user_review=review)
-            return data, status.HTTP_200_OK
-
-        except IntegrityError as e:
-            logger.exception(str(e))
-            return None, status.HTTP_409_CONFLICT
-
-        except Exception as e:
-            logger.exception(str(e))
-            return None, status.HTTP_500_INTERNAL_SERVER_ERROR
+        return self.create(movie=movie_obj, user=user_obj, user_review=review)
 
     @staticmethod
     def get_movie(title: str):
-        try:
-            data = Movie.objects.get(title=title)
-            return data, status.HTTP_200_OK
+        return Movie.objects.filter(title=title).last()
 
-        except ObjectDoesNotExist as e:
-            logger.exception(str(e))
-            return None, status.HTTP_404_NOT_FOUND
+    def get_review(self, user, movie):
+        return self.filter(movie=movie, user=user).last()
 
-        except Exception as e:
-            logger.exception(str(e))
-            return None, status.HTTP_500_INTERNAL_SERVER_ERROR
+    def delete_review(self, movie):
+        return self.filter(movie=movie).last().delete()
 
-    def get_review(self, movie):
-        try:
-            data = self.get(movie=movie)
-            return data, status.HTTP_200_OK
-
-        except ObjectDoesNotExist as e:
-            logger.exception(str(e))
-            return None, status.HTTP_404_NOT_FOUND
-
-        except Exception as e:
-            logger.exception(str(e))
-            return None, status.HTTP_500_INTERNAL_SERVER_ERROR
-
-    def delete_review(self, movie) -> (tuple, int):
-        try:
-            data = self.get(movie=movie).delete()
-            return data, status.HTTP_200_OK
-
-        except ObjectDoesNotExist as e:
-            logger.exception(str(e))
-            return None, status.HTTP_404_NOT_FOUND
-
-        except Exception as e:
-            logger.exception(str(e))
-            return None, status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
 
